0a0bdf6494f5aecbfd6f1f5d923ed527d7cd83a1
smichael1 <smichael1@cox.net>
2017-07-31 14:11:08 -0700

    validation through to UI complete

--- src/components/command_list.js
+++ src/components/command_list.js
@@ -4,6 +4,7 @@
 import React, {Component} from 'react';
 import CommandListItem from './command_list_item';
 import {connect} from 'react-redux';
+import NotificationSystem from 'react-notification-system';
 
 class CommandList extends Component {
 
@@ -12,6 +13,34 @@ class CommandList extends Component {
         super(props);
         this.renderList = this.renderList.bind(this);
     }
+
+    componentDidMount() {
+        this._notificationSystem = this.refs.notificationSystem;
+    }
+
+    // called once the component updated
+    componentDidUpdate() {
+
+        if (this.props.commandStatus.overallStatus == 'AllCompleted') {
+            // do it here
+            this._notificationSystem.addNotification({
+                title: 'Success',
+                message: 'The SetupConfig was successfully submitted',
+                level: 'success',
+                position: 'tl'
+            });
+
+        } else if (this.props.commandStatus.overallStatus == 'NotAccepted') {
+            this._notificationSystem.addNotification({
+                title: 'SetupConfig Error',
+                message: this.props.commandStatus.detail,
+                level: 'error',
+                position: 'tl'
+            });
+        }
+
+    }
+
 
     renderList() {
 
@@ -30,9 +59,12 @@ class CommandList extends Component {
     }
 
     render() {
+
+
     return (
         <form className="form-inline">
 
+            <NotificationSystem ref="notificationSystem"  />
             <table className="table">
                 <caption>Select a command and press Submit to send.</caption>
                 <thead>
